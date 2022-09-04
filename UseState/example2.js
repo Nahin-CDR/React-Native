4eb0d3c1c723162df195a92d36c8a60aa894b8c6
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2022-09-04 21:25:27 +0600

    Create example2.js

---
+++ UseState/example2.js
@@ -0,0 +1,39 @@
+import {View,Text,StyleSheet,Button,ScrollView} from 'react-native';
+import React,{useState} from 'react';
+import axios from 'axios';
+
+const TestPage = () => {
+
+     const [name,setName] = useState("Karim");
+     const [myData,setData] = useState([]);
+     const [category,setCategory] = useState("posts");
+     
+     useState(()=>{
+          axios.get(`https://jsonplaceholder.typicode.com/${category}`)
+          .then(function(response) {
+
+               setData(response.data);
+          })
+
+     },[myData]);
+
+     // [] eta use korar jonno etar vetore jara jara thakbe tarai just update hole useEffect er vetor er jinis 
+     // gula execute hobe
+
+     return (
+          <ScrollView>
+               <View>
+                    <Text style={{fontSize:40,marginLeft:20}}>Welcome!</Text>
+                    <Text style={{fontSize:30,color:'#000000',marginLeft:20}}>{name}</Text>
+                    <Button title="change name" onPress={()=>setName("React Native")} />
+                    {
+                         myData.map((data)=>{
+                              console.log(data.id); // data gula console e print hobe
+                         })
+                    }
+               </View>
+         </ScrollView>
+     );
+}
+
+export default TestPage;
