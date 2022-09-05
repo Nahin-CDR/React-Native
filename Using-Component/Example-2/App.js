adc7fc1f3c49996c3adb26f6a402398cddfeba07
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2022-09-05 23:48:33 +0600

    Update App.js

--- Using-Component/Example-2/App.js
+++ Using-Component/Example-2/App.js
@@ -1,3 +1,48 @@
-import { View, Text, StyleSheet,Button,ScrollView} from 'react-native';
 import React,{ useState, useEffect} from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
 import axios from 'axios';
+import DemoData from './data';
+import DisplayData from './display';
+
+const TestPage = () => {
+    
+
+    const [ApiData,setApiData] = useState([]);
+    useEffect(() => {
+     
+          axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+               //console.log(res.data);
+               setApiData(res.data);
+          });
+    },[]);
+
+
+
+
+    const listSeparator=()=>{
+          return <View style = {styles.seperator}/>
+    }
+     return(
+          <SafeAreaView>
+               <FlatList
+                    data = {ApiData}
+                    renderItem = {DisplayData}  
+                    ItemSeparatorComponent={ listSeparator }             
+               />
+          </SafeAreaView>
+     );
+}
+
+
+
+
+const styles = StyleSheet.create({
+     seperator:{
+          height: 1,
+          width: '100%',
+          marginTop: 10,
+          marginBottom:10,
+          backgroundColor: '#CCC',
+     }
+});
+export default TestPage;
