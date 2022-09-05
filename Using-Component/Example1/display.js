fb52f778c8037ac664d2bd498639677fe87d1015
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2022-09-05 22:11:17 +0600

    Create display.js

---
+++ Using-Component/Example1/display.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {View,StyleSheet,Text } from 'react-native';
+
+
+const DisplayData = ({item}) => { 
+     return (
+          <View>
+               <Text style = {styles.titleStyle}>{item.id}</Text>
+               <Text style = {styles.bodyStyle}>{item.title}</Text>
+          </View>
+     );
+}
+
+const styles = StyleSheet.create({
+
+     titleStyle:{
+          color: '#ff7f50', 
+          fontSize:30,
+          fontWeight: 'bold',
+          marginLeft : 10,
+     },
+     bodyStyle:{
+          color: '#000000', 
+          fontSize:15,
+          marginLeft:10,
+     }
+});
+export default DisplayData;
