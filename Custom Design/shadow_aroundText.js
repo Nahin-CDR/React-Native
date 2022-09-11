8dec36297e32e8a7e019299cf3e6abe7c1b07270
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2022-09-11 13:17:05 +0600

    Create shadow_aroundText.js

---
+++ Custom Design/shadow_aroundText.js
@@ -0,0 +1,16 @@
+<View style={{height :100}}>
+<Text 
+    style={{
+        color: "white", 
+        textShadowColor: 'black', 
+        textShadowOffset: { width: -1, height: 0 },
+        textShadowRadius: 5, 
+        fontSize: 20,
+        justifyContent: 'center', 
+        textAlign : 'center',
+        //fontWeight: '80'
+     }}
+    >
+   Nahin the Coder
+</Text>
+</View>
