a4353ff0a33e4b9ecbc1ef86ebd6b837217a6242
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2022-09-11 13:14:05 +0600

    Create black_shadow_around_view.js

---
+++ Custom Design/black_shadow_around_view.js
@@ -0,0 +1,15 @@
+ <View style={{ overflow: 'hidden', paddingBottom: 5 }}>
+                    <View
+                         style={{
+                         backgroundColor: '#fff',
+                         width: 150,
+                         height: 160,
+                         marginLeft :20,
+                         shadowColor: '#000',
+                         shadowOffset: { width: 1, height: 10 },
+                         shadowOpacity:  0.4,
+                         shadowRadius: 3,
+                         elevation: 5,
+                         }} 
+                    />
+</View>
