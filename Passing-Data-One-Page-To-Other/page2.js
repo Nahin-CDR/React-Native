8a460c5fbd85891651435b050e1ea7ac0a65b793
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2022-09-14 17:21:54 +0600

    Update page2.js

--- Passing-Data-One-Page-To-Other/page2.js
+++ Passing-Data-One-Page-To-Other/page2.js
@@ -8,9 +8,6 @@ import {
   navigation,
   SafeAreaView } from 'react-native';
 
-import CarouselCard from '../components/FlashCard/CarouselCard';
-export const S_WIDTH = Dimensions.get('window').width - 20;
-export const WIDTH = Math.round(WIDTH * 0.7);
 
 
 export default function App({ route, navigation }) {
@@ -32,31 +29,4 @@ export default function App({ route, navigation }) {
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-      backgroundColor: 'white',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: 30
-    },
-    appBarTitle:{
-      fontWeight : 'bold',
-      fontSize :25,
-      color : '#000000',
-      marginLeft:15,
-      alignItems:'center',
-      justifyContent: 'center'
-      
-    },
-    appBarBackground:{
-      backgroundColor :'white',
-      height :40,
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    line:{
-      backgroundColor :'#808080',
-      height : 1,
-      width : S_WIDTH,
-    }
-});
+
